e9134773dcfaa11997bc84ea403a2e79eed467a5
Vimalraj V <vimalrajvjkr@gmail.com>
2025-01-10 13:52:51 +0530

    Handled Loading and error State

--- app/src/main/java/com/vimalraj/todoapplication/movies/MovieApiClient.kt
+++ app/src/main/java/com/vimalraj/todoapplication/movies/MovieApiClient.kt
@@ -5,7 +5,8 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface MovieApiClient {
-    @GET("43c58e72-d161-422a-ba29-28762354b352")
+    //@GET("cbfdc7c1-1268-4694-b4d3-8434337b8738") // - > Error response 500
+    @GET("43c58e72-d161-422a-ba29-28762354b352") //- > For Valid 200 Response
     suspend fun getMoviesStatus(): Response<MoviesList>
 
 }
